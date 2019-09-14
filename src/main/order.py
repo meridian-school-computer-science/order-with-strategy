f792fb31e7098c4b8967c3897c49f02cd1790e64
kevmac64 <k_mac87@live.com>
2019-09-14 09:04:57 -0500

    Order entry finished

--- src/main/order.py
+++ src/main/order.py
@@ -3,10 +3,12 @@ from csv import DictReader
 
 class DataReader:
 
-    def __init__(self, target_order_list):
-        self.source_file = ''
+    def __init__(self, target_order_list, source_file):
+        self.source_file = source_file
         self.all_rows = []
         self.order_list = target_order_list
+        self.read_from_csv()
+        self.process_csv_to_order()
 
     def read_from_csv(self):
         with open(self.source_file) as csv_file:
@@ -17,10 +19,6 @@ class DataReader:
                     line_count += 1
                 self.all_rows.append(row)
                 line_count += 1
-
-    def __str__(self):
-        for i in self.all_rows:
-            print(i)
 
     def process_csv_to_order(self):
         for i in self.all_rows:
@@ -36,21 +34,34 @@ class AllOrders:
     def add_order(self, an_order):
         self.items.append(an_order)
 
+    def show_orders(self):
+        for each in self.items:
+            print(each)
+
 
 class Order:
 
-    def __init__(self, ordered_data):
-        self.ordered_data = ordered_data
-        self.build_order()
+    def __init__(self, one_order):
+        self.ordered_data = one_order
+        self.orderID = one_order['orderID']
+        self.cost = one_order['cost']
+        self.tax_status = one_order['tax status']
+        self.gift_card = one_order['gift card']
+        self.savings_code = one_order['savings code']
+        self.shipping_mode = one_order['shipping mode']
+        self.customer_name = one_order['customer name']
+        self.street_address = one_order['street address']
+        self.city = one_order['city']
+        self.state = one_order['state']
+        self.zip_code = one_order['zip code']
+        self.order_status = one_order['order status']
 
-    def build_order(self):
-        pass
+    def __str__(self):
+        return f"{self.orderID}: ${self.cost} status:{self.order_status}"
 
 
-order_list= AllOrders()
-d = DataReader(order_list)
-d.source_file = 'order data.csv'
-d.read_from_csv()
-print(d)
+order_list = AllOrders()
+d = DataReader(order_list, 'order data.csv')
+order_list.show_orders()
 
-# orderID,cost,tax status,gift card,savings code,shipping mode,customer name,street address,city,state,zip code,order status
+
